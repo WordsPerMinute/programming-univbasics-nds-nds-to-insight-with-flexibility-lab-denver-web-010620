853e4092bc11876d26949cdf036c734df5688e23
Corey Hodge <contact@coreyhodge.net>
2019-12-30 04:29:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   iMovieHashes = 0
-  AoH = []
+  newArray = []
   while iMovieHashes < movies_collection.length do
     AoH[:director_name] = movies_collection[iMovieHashes][:director_name]
     iMovieHashes += 1
